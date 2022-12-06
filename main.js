a6c374075bef6726fb5c094344f2f0c18bed4259
neekyjiang <neekyjiang@tencent.com>
2022-12-06 15:31:21 +0800

    ✨feature: 实现进程中间通信

--- main.js
+++ main.js
@@ -1,24 +1,39 @@
-const { app, BrowserWindow } = require('electron')
+let path = require("path")
+
+
+const { app, BrowserWindow, ipcMain} = require('electron')
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 1000,
         height: 800,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+          },
     })
-
+    
+    ipcMain.handle('ping', () => 'pong')
     win.loadFile('src/html/index.html')
+
+    // 打开窗口的检查
+    win.webContents.openDevTools()
 }
 
 app.whenReady().then(() => {
     createWindow()
 
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) { 
-            createWindow() 
-        }
-    })
+    // 不针对 mac 平台做特别的处理
+    // app.on('activate', () => {
+    //     if (BrowserWindow.getAllWindows().length === 0) { 
+    //         createWindow() 
+    //     }
+    // })
 })
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
+    // 不针对 mac 平台做特别的处理
+    // 关闭窗口就退出程序
+    //if (process.platform !== 'darwin') app.quit()
+    
+    app.quit()
 })
